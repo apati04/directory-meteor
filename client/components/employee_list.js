b0d6d0b1775c2c8a98216e39adfe2bbc4b351cb9
Andrew Patipaksiri <apax714@gmail.com>
2017-03-16 12:08:21 -0700

    add button to load more employees

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -3,6 +3,8 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 import EmployeeDetail from './employee_detail';
 
+const PER_PAGE = 20;
+
 const EmployeeList = (props) => {
 
   return (
@@ -12,13 +14,18 @@ const EmployeeList = (props) => {
           <EmployeeDetail key={employee._id} employee={employee} />
         )}
       </div>
+      <button
+        onClick={() => Meteor.subscribe('employees', 40) }
+        className="btn btn-primary">
+        Load More...
+      </button>
     </div>
   )
 }
 
 export default createContainer(()=> {
   // set up subscription
-  Meteor.subscribe('employees');
+  Meteor.subscribe('employees', PER_PAGE);
   // return an object, which will be sent to EmployeeList as props
   return { employees: Employees.find({}).fetch() };
 },EmployeeList);
