43a75c593e6ebab9befe655fd129b51d2b85e903
Andrew Patipaksiri <apax714@gmail.com>
2017-03-16 10:45:04 -0700

    generate number of records for employee directory

--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
+import _ from 'lodash';
 import { Meteor } from 'meteor/meteor';
 import { Employees } from '../imports/collections/employees';
+import { image, helpers } from 'faker';
 
 Meteor.startup(() => {
   // generate data
@@ -9,5 +11,13 @@ Meteor.startup(() => {
   const numberRecords = Employees.find({}).count();
   if(!numberRecords) {
     // Generate some data...
+    _.times(5000, () => {
+      const { name, email, phone } = helpers.createCard();
+
+      Employees.insert({
+        name, email, phone,
+        avatar: image.avatar()
+      });
+    });
   }
 });
