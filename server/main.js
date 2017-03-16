28b51d36473e8b747c651f2fb2febe22d6b78eb9
Andrew Patipaksiri <apax714@gmail.com>
2017-03-16 08:59:55 -0700

    checks to see if there is any data in collections

--- server/main.js
+++ server/main.js
@@ -1,5 +1,13 @@
 import { Meteor } from 'meteor/meteor';
+import { Employees } from '../imports/collections/employees';
 
 Meteor.startup(() => {
+  // generate data
 
+  // check if data exists in collection
+  // see if collection has any records
+  const numberRecords = Employees.find({}).count();
+  if(!numberRecords) {
+    // Generate some data...
+  }
 });
